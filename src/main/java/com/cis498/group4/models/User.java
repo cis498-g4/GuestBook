3df9d2261c8a3054927df10e88401e9196528610
Mike Molenda <molenda.mike@gmail.com>
2017-04-05 14:34:26 -0500

    Set up model template for User

---
+++ src/main/java/com/cis498/group4/models/User.java
@@ -0,0 +1,73 @@
+/**
+ * User
+ * JavaBean representing a user
+ */
+
+package com.cis498.group4.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    public enum UserType {ADMIN, PRESENTER, GUEST}
+
+    private int id;
+    private UserType type;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
