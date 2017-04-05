b474770d3ae7c39cf86675aeeacdf830959e4958
Mike Molenda <molenda.mike@gmail.com>
2017-04-04 20:47:09 -0500

    Add package structure for controllers and models, and test servlet

---
+++ src/main/java/com/cis498/group4/controllers/TestServlet.java
@@ -0,0 +1,23 @@
+package com.cis498.group4.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<b>It works!</b>");
+        out.println("</html>");
+    }
+}
