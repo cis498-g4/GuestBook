919bb0a897152a9a7434806deb01365cad0114a6
Mike Molenda <molenda.mike@gmail.com>
2017-04-05 09:03:04 -0500

    Add doGet redirect to TestServlet.doPost()

--- src/main/java/com/cis498/group4/controllers/TestServlet.java
+++ src/main/java/com/cis498/group4/controllers/TestServlet.java
@@ -11,13 +11,14 @@ import java.io.PrintWriter;
 @WebServlet("/test")
 public class TestServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         out.println("<html>");
-        out.println("<b>It works!</b>");
+        out.println("It works!");
         out.println("</html>");
     }
+
 }
