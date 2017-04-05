bd4186c3dc8ea209f0b2e0894c30691219076ce0
Mike Molenda <molenda.mike@gmail.com>
2017-04-05 12:02:47 -0500

    Simplify TestServlet

--- src/main/java/com/cis498/group4/controllers/TestServlet.java
+++ src/main/java/com/cis498/group4/controllers/TestServlet.java
@@ -1,3 +1,8 @@
+/**
+ * TestServlet
+ * Responds with a simple HTTP message
+ */
+
 package com.cis498.group4.controllers;
 
 import javax.servlet.ServletException;
@@ -10,9 +15,6 @@ import java.io.PrintWriter;
 
 @WebServlet("/test")
 public class TestServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
