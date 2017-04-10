6ee5d129241a8b61b99594a4cbd5d68cb65c76d2
Mike Molenda <molenda.mike@gmail.com>
2017-04-10 08:55:22 -0500

    Add DbConn utility class to facilitate DB connections

---
+++ src/main/java/com/cis498/group4/util/DbConn.java
@@ -0,0 +1,58 @@
+package com.cis498.group4.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * The DbConn class handles the database connection to the MySQL server.
+ * Connection properties are read from the file, /src/main/java/db.properties.
+ * Change the properties in this file to match your MySQL instance.
+ */
+public class DbConn {
+
+    private static Connection connection = null;
+
+    /**
+     * Creates and returns a new connection  to the MySQL instance,
+     * or returns the current connection if one exists.
+     * @return connection - A connection to the MySQL instance
+     */
+    public static Connection getConnection() {
+        // TODO: simplify logic: if connection null, build new; only one return statement
+        if (connection != null) {
+            return connection;
+        } else {
+            try {
+                // Load connection properties from db.properties file
+                Properties properties = new Properties();
+                InputStream inputStream = DbConn.class.getClassLoader().getResourceAsStream("/db.properties");
+                properties.load(inputStream);
+
+                // Create and new connection with properties
+                String driver = properties.getProperty("driver");
+                String url = properties.getProperty("url");
+                String user = properties.getProperty("user");
+                String password = properties.getProperty("password");
+
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return connection;
+
+        }
+
+    }
+
+}
