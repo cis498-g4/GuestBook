1377a868c2098c39cac635387cec914929e4b6e0
Mike Molenda <molenda.mike@gmail.com>
2017-04-09 19:33:49 -0500

    Create model object for Survey

---
+++ src/main/java/com/cis498/group4/models/Survey.java
@@ -0,0 +1,63 @@
+package com.cis498.group4.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Survey
+ * JavaBean representing a response to a survey
+ */
+public class Survey implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    private int id;
+    private User user;
+    private Event event;
+    private LocalDateTime submissionDateTime;
+    private List<Integer> responses;
+
+    public Survey() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public LocalDateTime getSubmissionDateTime() {
+        return submissionDateTime;
+    }
+
+    public void setSubmissionDateTime(LocalDateTime submissionDateTime) {
+        this.submissionDateTime = submissionDateTime;
+    }
+
+    public List<Integer> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(List<Integer> responses) {
+        this.responses = responses;
+    }
+
+}
