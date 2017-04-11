933588442410afdb8b122464d591f4c674375d82
Mike Molenda <molenda.mike@gmail.com>
2017-04-11 10:32:04 -0500

    Rename IOHelpers to IoHelpers

--- src/main/java/com/cis498/group4/util/IOHelpers.java
+++
@@ -1,8 +0,0 @@
-package com.cis498.group4.util;
-
-/**
- * The IOHelpers class contains methods to aid with reading and writing to files
- */
-public class IOHelpers {
-
-}
---
+++ src/main/java/com/cis498/group4/util/IoHelpers.java
@@ -0,0 +1,8 @@
+package com.cis498.group4.util;
+
+/**
+ * The IoHelpers class contains methods to aid with reading and writing to files
+ */
+public class IoHelpers {
+
+}
