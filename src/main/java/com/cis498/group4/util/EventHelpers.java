6817ccf4e5dc09fc66ed8aef3ab5c96cb4994f97
Mike Molenda <molenda.mike@gmail.com>
2017-04-11 21:55:58 -0500

    Add empty static methods for checking event overlaps and checking event capacity

--- src/main/java/com/cis498/group4/util/EventHelpers.java
+++ src/main/java/com/cis498/group4/util/EventHelpers.java
@@ -1,8 +1,36 @@
 package com.cis498.group4.util;
 
+import com.cis498.group4.models.Event;
+
 /**
  * The EventHelpers class contains methods to assist with Event data (verification, etc)
  */
 public class EventHelpers {
+
+    /**
+     * Checks whether the event overlaps with an existing event with the same presenter
+     * @param event
+     * @return
+     */
+    public static boolean isOverlapping(Event event) {
+        boolean overlap = false;
+
+        // TODO
+
+        return overlap;
+    }
+
+    /**
+     * Checks whether an event is at capacity
+     * @param event
+     * @return
+     */
+    public static boolean isAtCapacity(Event event) {
+        boolean atCapacity = false;
+
+        // TODO
+
+        return atCapacity;
+    }
 
 }
