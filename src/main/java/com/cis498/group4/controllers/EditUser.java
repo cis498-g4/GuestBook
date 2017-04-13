f2c97ca70230f1df6c619209f178f266beac1767
Mike Molenda <molenda.mike@gmail.com>
2017-04-13 15:43:03 -0500

    Add edit user controller

---
+++ src/main/java/com/cis498/group4/controllers/EditUser.java
@@ -0,0 +1,70 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.UserDataAccess;
+import com.cis498.group4.models.User;
+import com.cis498.group4.util.UserHelpers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * The EditUser servlet responds to requests to edit a user's information.
+ */
+@WebServlet(name = "EditUser", urlPatterns = "/manager/edit-user")
+public class EditUser extends HttpServlet {private static final long serialVersionUID = 1L;
+
+    private UserDataAccess userData;
+
+    public EditUser() {
+        super();
+        userData = new UserDataAccess();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/views/edit-user.jsp";
+
+        User user = userData.getUser(Integer.parseInt(request.getParameter("id")));
+        request.setAttribute("user", user);
+
+        String pageTitle = String.format("Edit info for user %s %s", user.getFirstName(), user.getLastName());
+        request.setAttribute("pageTitle", pageTitle);
+
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Update information
+        User user = new User();
+        user.setId(Integer.parseInt(request.getParameter("id")));
+        user.setType(User.UserType.valueOf(request.getParameter("type").trim()));
+        user.setFirstName(request.getParameter("first-name"));
+        user.setLastName(request.getParameter("last-name"));
+        user.setEmail(request.getParameter("email"));
+
+        String url = "/manager/list-users";
+        String statusMessage;
+
+        // TODO: Validate user info before commit
+        if (UserHelpers.validateUser(user) || true /* TODO: remove this!!! */) {
+            userData.updateUser(user);
+            statusMessage = "User information updated successfully.";
+        } else {
+            statusMessage = "User update failed!";
+        }
+
+        request.setAttribute("statusMessage", statusMessage);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+}
