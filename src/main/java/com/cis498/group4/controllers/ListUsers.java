58488ff940a0f6cd20143714f3cd74f7cd82a55a
Mike Molenda <molenda.mike@gmail.com>
2017-04-13 15:43:28 -0500

    Add doPost —> doGet redirect

--- src/main/java/com/cis498/group4/controllers/ListUsers.java
+++ src/main/java/com/cis498/group4/controllers/ListUsers.java
@@ -43,7 +43,7 @@ public class ListUsers extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
 }
