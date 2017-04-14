0a98eea0afc2e95009be26a476189a2c350315a3
Mike Molenda <molenda.mike@gmail.com>
2017-04-14 14:31:08 -0500

    Add controller and view for Add Event

---
+++ src/main/java/com/cis498/group4/controllers/AddEvent.java
@@ -0,0 +1,110 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.EventDataAccess;
+import com.cis498.group4.data.UserDataAccess;
+import com.cis498.group4.models.Event;
+import com.cis498.group4.models.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * The AddEvent servlet responds to requests to add a new event.
+ */
+@WebServlet(name = "AddEvent", urlPatterns="/manager/add-event")
+public class AddEvent extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private EventDataAccess eventData;
+    private UserDataAccess userData;
+
+    public AddEvent() {
+        super();
+        eventData = new EventDataAccess();
+        userData = new UserDataAccess();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/views/add-event.jsp";
+        String pageTitle = "Add new event";
+
+        List<User> organizers = userData.getOrganizers();
+        request.setAttribute("organizers", organizers);
+
+        request.setAttribute("pageTitle", pageTitle);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/manager/list-events";
+        String statusMessage;
+
+        // Create new event with form information
+
+        // TODO: input format must be YYYY-MM-DDTHH:MM - eventually use jQuery for this (http://bit.ly/2ozo93R)
+        String startInput = request.getParameter("start-date") + "T" + request.getParameter("start-time");
+        LocalDateTime startDt = LocalDateTime.parse(startInput);
+
+        String endInput = request.getParameter("end-date") + "T" + request.getParameter("end-time");
+        LocalDateTime endDt = LocalDateTime.parse(endInput);
+
+        if (startDt.isAfter(LocalDateTime.now())) {
+            if (startDt.isBefore(endDt)) {
+
+                Event event = new Event();
+                event.setName(request.getParameter("name"));
+                event.setStartDateTime(startDt);
+                event.setEndDateTime(endDt);
+
+                // Assume user ID is valid - chosen from selection box
+                User presenter = new User();
+                presenter.setId(Integer.parseInt(request.getParameter("pres-id")));
+                event.setPresenter(presenter);
+
+                event.setOpenRegistration(request.getParameter("open-reg") != null);
+                event.setRegistrationCode(request.getParameter("reg-code"));
+                event.setMandatorySurvey(request.getParameter("survey-req") != null);
+                event.setCapacity(Integer.parseInt(request.getParameter("capacity")));
+
+                // Attempt write to DB and respond to event
+                // TODO: Validate event info before commit (http://red.ht/2nMrGNu)
+                if (true) {
+
+                    int insertStatus = eventData.insertEvent(event);
+
+                    if (insertStatus == 0) {
+                        statusMessage = "Event created successfully.";  // FIXME: Status message shown but event not added
+                    } else {
+                        statusMessage = "ERROR: Add event operation failed!";
+                    }
+
+                } else {
+                    statusMessage = "ERROR: Invalid data entered for new event!";
+                }
+
+            } else {
+                statusMessage = "ERROR: Event end time occurs before event start time!";
+            }
+
+        } else {
+            statusMessage = "ERROR: Start time occurs in the past!";
+        }
+
+        request.setAttribute("statusMessage", statusMessage);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+}
---
+++ src/main/webapp/views/add-event.jsp
@@ -0,0 +1,41 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="/templates/header.jsp"></jsp:include>
+
+<!-- TODO: HTML / JS form validation -->
+<form action="add-event" method="post">
+    <label for="name">Event Name:</label>
+    <input type="text" name="name" id="name" required><br>
+    <label for="start-date">Start date:</label>
+    <input type="text" name="start-date" id="start-date" required><br>
+    <label for="start-time">Start time:</label>
+    <input type="text" name="start-time" id="start-time" required><br>
+    <label for="end-date">End date:</label>
+    <input type="text" name="end-date" id="end-date" required><br>
+    <label for="end-time">End time:</label>
+    <input type="text" name="end-time" id="end-time" required><br>
+    <label for="pres-id">Presenter:</label>
+    <select name="pres-id" id="pres-id">
+
+        <c:forEach items="${organizers}" var="organizer">
+        <option value="${organizer.id}">${organizer.firstName} ${organizer.lastName}</option>
+        </c:forEach>
+
+    </select><br>
+    <label for="capacity">Event capacity:</label>
+    <input type="text" name="capacity" id="capacity"><br>
+    <input type="checkbox" name="open-reg" id="open-reg">Allow open registration<br>
+    <label for="reg-code">Registration code (optional):</label>
+    <input type="text" name="reg-code" id="reg-code"><br>
+    <input type="checkbox" name="survey-req" id="survey-req">Require survey completion to record attendance<br>
+    <input type="submit" value="create event">
+</form>
+
+<hr>
+
+<button onclick="history.go(-1)">back</button>
+
+</body>
+</html>
+
