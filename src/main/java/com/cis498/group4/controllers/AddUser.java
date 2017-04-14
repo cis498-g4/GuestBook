92e4ac0fb0c8b88119d4ee40072377cf3d51eea0
Mike Molenda <molenda.mike@gmail.com>
2017-04-14 09:36:23 -0500

    Add controller and view for Add User

---
+++ src/main/java/com/cis498/group4/controllers/AddUser.java
@@ -0,0 +1,67 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.UserDataAccess;
+import com.cis498.group4.models.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * The AddUser servlet responds to requests to add a new user.
+ */
+@WebServlet(name = "AddUser", urlPatterns="/manager/add-user")
+public class AddUser extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private UserDataAccess userData;
+
+    public AddUser() {
+        super();
+        userData = new UserDataAccess();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/views/add-user.jsp";
+        String pageTitle = "Add new user";
+        request.setAttribute("pageTitle", pageTitle);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Create new user with form information
+        User user = new User();
+        user.setType(User.UserType.valueOf(request.getParameter("type").trim()));
+        user.setFirstName(request.getParameter("first-name"));
+        user.setLastName(request.getParameter("last-name"));
+        user.setEmail(request.getParameter("email"));
+        user.setPassword(request.getParameter("password"));
+
+        // Attempt write to DB and respond to user
+        String url = "/manager/list-users";
+        String statusMessage;
+
+        // TODO: Validate user info before commit (http://red.ht/2nMrGNu)
+        if (true) {
+            userData.insertUser(user);  // TODO: Make read/write operations return booleans or status codes?
+            statusMessage = String.format("New user %s %s was created successfully.",
+                    user.getFirstName(), user.getLastName());
+        } else {
+            statusMessage = "Add user operation failed!";
+        }
+
+        request.setAttribute("statusMessage", statusMessage);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+}
---
+++ src/main/webapp/views/add-user.jsp
@@ -0,0 +1,32 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="/templates/header.jsp"></jsp:include>
+
+<!-- TODO: HTML / JS form validation -->
+<form action="add-user" method="post">
+    <label for="first-name">First Name:</label>
+    <input type="text" name="first-name" id="first-name" required><br>
+    <label for="last-name">Last Name:</label>
+    <input type="text" name="last-name" id="last-name" required><br>
+    <label for="email">Email Address:</label>
+    <input type="email" name="email" id="email" required><br>
+    <label for="type">User type:</label>
+    <select name="type" id="type" selected="GUEST">
+        <option value="ORGANIZER">Organizer</option>
+        <option value="GUEST">Guest</option>
+    </select><br>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" required><br>
+    <label for="pwd-conf">Retype Password</label>
+    <input type="password" name="pwd-conf" id="pwd-conf" required><br>
+    <input type="submit" value="create user">
+</form>
+
+<hr>
+
+<button onclick="history.go(-1)">back</button>
+
+</body>
+</html>
+
