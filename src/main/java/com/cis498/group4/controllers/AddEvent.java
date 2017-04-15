1707f49dd59850fcc9cedf17e39d51fdbd78d761
Mike Molenda <molenda.mike@gmail.com>
2017-04-15 17:40:00 -0500

    Remove unnecessary imports

--- src/main/java/com/cis498/group4/controllers/AddEvent.java
+++ src/main/java/com/cis498/group4/controllers/AddEvent.java
@@ -12,9 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
