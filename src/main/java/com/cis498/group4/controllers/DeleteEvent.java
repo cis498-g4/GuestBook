fc612c72e5b1678c2290a58054e929f11770a4d8
Mike Molenda <molenda.mike@gmail.com>
2017-04-15 16:58:44 -0500

    Add controller and view for Delete Event

---
+++ src/main/java/com/cis498/group4/controllers/DeleteEvent.java
@@ -0,0 +1,75 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.EventDataAccess;
+import com.cis498.group4.models.Event;
+import com.cis498.group4.util.EventHelpers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * The DeleteEvent servlet responds to requests to delete a event.
+ */
+@WebServlet(name = "DeleteEvent", urlPatterns = "/manager/delete-event")
+public class DeleteEvent extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private EventDataAccess eventData;
+
+    public DeleteEvent() {
+        super();
+        eventData = new EventDataAccess();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/views/delete-event.jsp";
+
+        Event event = eventData.getEvent(Integer.parseInt(request.getParameter("id")));
+        request.setAttribute("event", event);
+
+        String pageTitle = String.format("Delete event \"%s\"?", event.getName());
+        request.setAttribute("pageTitle", pageTitle);
+
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String url = "/manager/list-events";
+        String statusMessage;
+
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        Event event = eventData.getEvent(id);
+
+        if (event.getStartDateTime().isAfter(LocalDateTime.now())) {
+
+            int deleteStatus = eventData.deleteEvent(id);
+
+            if (deleteStatus == 0) {
+                statusMessage = "Event has been removed";
+            } else if (deleteStatus == 1451) {
+                statusMessage = "ERROR: Cannot delete a event that is associated with surveys or registrations!";
+            } else {
+                statusMessage = "ERROR: Delete event operation failed!";
+            }
+
+        } else {
+            statusMessage = "ERROR: Cannot delete an event that has already occurred!";
+        }
+
+        request.setAttribute("statusMessage", statusMessage);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+}
---
+++ src/main/webapp/views/delete-event.jsp
@@ -0,0 +1,65 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="/templates/header.jsp"></jsp:include>
+
+<p>The following event will be <strong>permanently</strong> removed:</p>
+
+<table>
+    <tr>
+        <th>Event Name:</th>
+        <td>${event.name}</td>
+    </tr>
+    <tr>
+        <th>Event Start:</th>
+        <td>
+            ${event.startDateTime.getMonthValue()}/${event.startDateTime.getDayOfMonth()}/${event.startDateTime.getYear()}
+            ${event.startDateTime.getHour()}:${event.startDateTime.getMinute() < 10 ? "0" : "&nbsp;"}${event.startDateTime.getMinute()}
+        </td>
+    </tr>
+    <tr>
+        <th>Event End:</th>
+        <td>
+            ${event.endDateTime.getMonthValue()}/${event.endDateTime.getDayOfMonth()}/${event.endDateTime.getYear()}
+            ${event.endDateTime.getHour()}:${event.endDateTime.getMinute() < 10 ? "0" : "&nbsp;"}${event.endDateTime.getMinute()}
+        </td>
+    </tr>
+    <tr>
+        <th>Presenter:</th>
+        <td>${event.presenter.lastName}, ${event.presenter.firstName}</td>
+    </tr>
+    <tr>
+        <th>Registration Type:</th>
+        <td>${event.openRegistration ? "Open" : "Closed"}</td>
+    </tr>
+    <tr>
+        <th>Registration Code:</th>
+        <td>${event.registrationCode != null ? event.registrationCode : "<em>none</em>"}</td>
+    </tr>
+    <tr>
+        <th>Survey Required:</th>
+        <td>${event.mandatorySurvey ? "Yes" : "No"}</td>
+    </tr>
+    <tr>
+        <th>Max Capacity:</th>
+        <td>${event.capacity > 0 ? event.capacity : "<em>none</em>"}</td>
+    </tr>
+</table>
+
+<form action="delete-event" method="post">
+    <p>
+        This effectively cancels the event. Are you sure?<br>
+        <input type="hidden" name="id" value="${event.id}"><br>
+        <input type="submit" value="confirm delete">
+        <button onclick="history.go(-1)">cancel</button>
+    </p>
+</form>
+
+<hr>
+
+<button onclick="history.go(-1)">back</button>
+
+</body>
+</html>
+
+
