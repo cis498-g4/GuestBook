f064b2e207e097ee391623f32c80465bbc9c5c4b
Mike Molenda <molenda.mike@gmail.com>
2017-04-16 18:37:08 -0500

    Update hash value in test data

--- db/testdata.sql
+++ db/testdata.sql
@@ -1,11 +1,11 @@
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Perwaize', 'Ahmed', 'PerwaizeAhmed2013@u.northwestern.edu', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5'); -- sha-1 of 'cis498'
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Matt', 'Granum', 'MatthewGranum2017@u.northwestern.edu', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Scott', 'Langnas', 'ScottLangnas2018@u.northwestern.edu', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Mike', 'Molenda', 'm-molenda@northwestern.edu', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Albert', 'Einstein', 'albert@cis498.com', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Marie', 'Curie', 'marie@cis498.com', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Isaac', 'Newton', 'isaac@cis498.com', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
-INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Stephen', 'Hawking', 'stephen@cis498.com', '8e52c6ae8abc5d8506fffebd5a7773c5535495d5');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Perwaize', 'Ahmed', 'PerwaizeAhmed2013@u.northwestern.edu', '382d65224bda884212c3f86b8b85291cdbe653b9'); -- hash value of 'cis498'
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Matt', 'Granum', 'MatthewGranum2017@u.northwestern.edu', '382d65224bda884212c3f86b8b85291cdbe653b9');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Scott', 'Langnas', 'ScottLangnas2018@u.northwestern.edu', '382d65224bda884212c3f86b8b85291cdbe653b9');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (0, 'Mike', 'Molenda', 'm-molenda@northwestern.edu', '382d65224bda884212c3f86b8b85291cdbe653b9');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Albert', 'Einstein', 'albert@cis498.com', '382d65224bda884212c3f86b8b85291cdbe653b9');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Marie', 'Curie', 'marie@cis498.com', '382d65224bda884212c3f86b8b85291cdbe653b9');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Isaac', 'Newton', 'isaac@cis498.com', '382d65224bda884212c3f86b8b85291cdbe653b9');
+INSERT INTO `guestbookdb`.`user`(`user_type_id`, `first_name`, `last_name`, `email`, `password`) VALUES (1, 'Stephen', 'Hawking', 'stephen@cis498.com', '382d65224bda884212c3f86b8b85291cdbe653b9');
 
 
 INSERT INTO `guestbookdb`.`event`(`event_name`, `start_date_time`, `end_date_time`, `presenter_id`, `registration_code`, `open_registration`, `mandatory_survey`, `capacity`) VALUES ('Building Webpages', '2017-07-01 10:00:00', '2017-07-01 11:00:00', 1, 'WEBD1111', TRUE, TRUE, 100);
