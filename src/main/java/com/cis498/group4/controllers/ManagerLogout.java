0e693673dbbfbd1d4f576f5f8462beb0c16bf564
Mike Molenda <molenda.mike@gmail.com>
2017-04-18 13:11:28 -0500

    Create logout servlet

---
+++ src/main/java/com/cis498/group4/controllers/ManagerLogout.java
@@ -0,0 +1,49 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.UserDataAccess;
+import com.cis498.group4.models.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * The ManagerLogout servlet responds to requests to log out of the management console.
+ */
+@WebServlet(name = "ManagerLogout", urlPatterns = "/manager/logout")
+public class ManagerLogout extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public ManagerLogout() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Invalidate session
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        // Redirect to login
+        String url = "/WEB-INF/views/manager-login.jsp";
+        String pageTitle = "Management Console Login";
+        String statusMessage = "Successfully logged out";
+
+        request.setAttribute("pageTitle", pageTitle);
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
