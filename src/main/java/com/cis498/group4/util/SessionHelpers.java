23894cc4f2adc6f8d1116d9a46ce65c7e4d47ed4
Mike Molenda <molenda.mike@gmail.com>
2017-04-18 09:12:51 -0500

    Add helpers to store and retrieve session user info

--- src/main/java/com/cis498/group4/util/SessionHelpers.java
+++ src/main/java/com/cis498/group4/util/SessionHelpers.java
@@ -1,8 +1,30 @@
 package com.cis498.group4.util;
 
+import com.cis498.group4.models.User;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * The SessionHelpers class contains methods to help deal with session data
  */
 public class SessionHelpers {
+
+    /**
+     * Retrieves the user info stored in the current session
+     * @param session The current session
+     * @return The user stored in the session
+     */
+    public static User getSessionUser(HttpSession session) {
+        return (User) session.getAttribute("sessionUser");
+    }
+
+    /**
+     * Stores the specified user in the session
+     * @param session The current session
+     * @param user The user to store
+     */
+    public static void setSessionUser(HttpSession session, User user) {
+        session.setAttribute("sessionUser", user);
+    }
 
 }
