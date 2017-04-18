62c97e5817fd24612ba32e41d0879a05d3e4bf85
Mike Molenda <molenda.mike@gmail.com>
2017-04-18 15:44:37 -0500

    Remove unnecessary imports

--- src/main/java/com/cis498/group4/controllers/EventRegistrationList.java
+++ src/main/java/com/cis498/group4/controllers/EventRegistrationList.java
@@ -2,7 +2,6 @@ package com.cis498.group4.controllers;
 
 import com.cis498.group4.data.AttendanceDataAccess;
 import com.cis498.group4.data.EventDataAccess;
-import com.cis498.group4.data.UserDataAccess;
 import com.cis498.group4.models.Event;
 import com.cis498.group4.util.SessionHelpers;
 
@@ -13,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
