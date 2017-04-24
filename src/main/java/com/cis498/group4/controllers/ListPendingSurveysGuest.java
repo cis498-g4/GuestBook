5261c1f9eb6ebf312ea363e69f20a19bd6cbebf7
Mike Molenda <molenda.mike@gmail.com>
2017-04-24 12:37:31 -0500

    Add controller and view to list pending surveys for a guest

---
+++ src/main/java/com/cis498/group4/controllers/ListPendingSurveysGuest.java
@@ -0,0 +1,76 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.AttendanceDataAccess;
+import com.cis498.group4.models.Attendance;
+import com.cis498.group4.models.Event;
+import com.cis498.group4.models.User;
+import com.cis498.group4.util.SessionHelpers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The ListPendingSurveysGuest servlet responds to requests to show events where the user has not yet taken a survey.
+ */
+@WebServlet(name = "ListPendingSurveysGuest", urlPatterns = "/manager/list-surveys-guest")
+public class ListPendingSurveysGuest extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private AttendanceDataAccess attendanceData;
+
+    public ListPendingSurveysGuest() {
+        super();
+        attendanceData = new AttendanceDataAccess();
+    }
+
+    /**
+     * Renders a list of all events for which the current user has attended or signed in, but has not completed a survey
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Restrict access by non-Guests
+        if (!SessionHelpers.checkGuest(request.getSession())) {
+            response.sendError(
+                    HttpServletResponse.SC_FORBIDDEN, "You do not have permission to access this resource");
+            return;
+        }
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("sessionUser");
+
+        String url = "/WEB-INF/views/list-surveys-guest.jsp";
+        String pageTitle = "Pending Surveys";
+        request.setAttribute("pageTitle", pageTitle);
+
+        List<Attendance> pendingSurveys = attendanceData.getPendingSurveys(user);
+        List<Event> pendingSurveyEvents = new ArrayList<Event>();
+
+        for (Attendance attendance : pendingSurveys) {
+            pendingSurveyEvents.add(attendance.getEvent());
+        }
+
+        request.setAttribute("events", pendingSurveyEvents);
+
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/list-surveys-guest.jsp
@@ -0,0 +1,54 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="/WEB-INF/templates/header.jsp"></jsp:include>
+
+<table>
+    <tr>
+        <th>Event Name</th>
+        <th>Date</th>
+        <th>Presenter</th>
+        <th>Survey Required</th>
+    </tr>
+
+    <c:if test="${events.isEmpty()}">
+        <tr>
+            <td colspan="5" align="center">No events found</td>
+        </tr>
+    </c:if>
+
+    <c:forEach items="${events}" var="event">
+        <tr>
+            <td>${event.name}</td>
+            <td>${event.startDateTime.getMonthValue()}/${event.startDateTime.getDayOfMonth()}/${event.startDateTime.getYear()}</td>
+            <td>${event.presenter.lastName}, ${event.presenter.firstName}</td>
+            <td>${event.mandatorySurvey ? "<strong>Yes</strong>" : "No"}</td>
+            <td>
+                <form action="take-survey">
+                    <input type="hidden" name="eventId" value="${event.id}">
+                    <input type="submit" value="take survey">
+                </form>
+            </td>
+        </tr>
+    </c:forEach>
+
+</table>
+
+<hr>
+
+<form action="filter-surveys-user">
+    <label for="field">Filter by: </label>
+    <select name="field" id="field">
+        <option>Event Name</option>
+        <option>Presenter Last Name</option>
+        <option>Event Date</option>
+        <option>Survey Required</option>
+    </select>
+    <input type="text" name="value">
+    <input type="checkbox" name="exact" checked>Exact matches only
+    <input type="submit" value="submit">
+</form>
+
+
+</body>
+</html>
