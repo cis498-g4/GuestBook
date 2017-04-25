ff18a06f9b1a3f208fbfe60fb40647baada9848b
Mike Molenda <molenda.mike@gmail.com>
2017-04-25 11:06:40 -0500

    Change restriction level to Organizers-only

--- src/main/java/com/cis498/group4/controllers/ShowEventInfo.java
+++ src/main/java/com/cis498/group4/controllers/ShowEventInfo.java
@@ -36,10 +36,10 @@ public class ShowEventInfo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Restrict access if not logged in
-        if (!SessionHelpers.checkLogin(request.getSession())) {
+        // Restrict access by non-Organizers
+        if (!SessionHelpers.checkOrganizer(request.getSession())) {
             response.sendError(
-                    HttpServletResponse.SC_FORBIDDEN, "You must login to access this resource");
+                    HttpServletResponse.SC_FORBIDDEN, "You do not have permission to access this resource");
             return;
         }
 
