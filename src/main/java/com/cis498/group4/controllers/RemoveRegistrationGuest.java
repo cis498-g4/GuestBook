d76afeed5d38ceb0acbce5ef405c7d7d4879c0f3
Mike Molenda <molenda.mike@gmail.com>
2017-04-25 15:11:54 -0500

    Create controller and view for remove registration guest, Implement doGet

---
+++ src/main/java/com/cis498/group4/controllers/RemoveRegistrationGuest.java
@@ -0,0 +1,147 @@
+package com.cis498.group4.controllers;
+
+import com.cis498.group4.data.AttendanceDataAccess;
+import com.cis498.group4.models.Attendance;
+import com.cis498.group4.models.Event;
+import com.cis498.group4.models.User;
+import com.cis498.group4.util.EventHelpers;
+import com.cis498.group4.util.SessionHelpers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * The RemoveRegistrationGuest servlet responds to requests to remove a guest registration from an event.
+ * This is accomplished by deleting an Event Attendance record from the DB.
+ */
+@WebServlet(name = "RemoveRegistrationGuest", urlPatterns = "/manager/remove-registration-guest")
+public class RemoveRegistrationGuest extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private AttendanceDataAccess attendanceData;
+
+    public RemoveRegistrationGuest() {
+        super();
+        attendanceData = new AttendanceDataAccess();
+    }
+
+    /**
+     * Render a confirmation message for the registration record to be removed
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Restrict access by non-Guests
+        if (!SessionHelpers.checkGuest(request.getSession())) {
+            response.sendError(
+                    HttpServletResponse.SC_FORBIDDEN, "You do not have permission to access this resource");
+            return;
+        }
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("sessionUser");
+
+        String url = "/WEB-INF/views/remove-registration-guest.jsp";
+
+        Attendance attendance = attendanceData.getAttendance(
+                user.getId(), Integer.parseInt(request.getParameter("eventId")));
+        request.setAttribute("attendance", attendance);
+
+        Event event = attendance.getEvent();
+        request.setAttribute("event", event);
+        String eventDate = event.getStartDateTime().format(DateTimeFormatter.ofPattern("MM/dd/yy"));
+        request.setAttribute("eventDate", eventDate);
+        String eventLongDate = event.getStartDateTime().format(DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy"));
+        request.setAttribute("eventLongDate", eventLongDate);
+
+        // Set warning message
+        String warningMessage;
+
+        if (!event.isOpenRegistration()) {
+            warningMessage = "<strong>You will not be able to re-register for this event " +
+                             "without the assistance of an event organizer!</strong>";
+        } else {
+            if (event.getRegistrationCode() != null) {
+                warningMessage = String.format(
+                        "If you wish to re-register, use the registration code <strong>%s</strong>.",
+                        event.getRegistrationCode());
+            } else {
+                warningMessage = "This event has open registration but no registration code. " +
+                                 "You may still sign in freely at the event kiosk.";
+            }
+        }
+
+        request.setAttribute("warningMessage", warningMessage);
+
+        String pageTitle = String.format("Remove your registration from %s (%s)?", event.getName(), eventDate);
+        request.setAttribute("pageTitle", pageTitle);
+
+        RequestDispatcher view = request.getRequestDispatcher(url);
+        view.forward(request, response);
+
+    }
+
+    /**
+     * Receive deletion confirmation, process the deletion in the database, and respond with a confirmation message
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Restrict access by non-Guests
+        if (!SessionHelpers.checkGuest(request.getSession())) {
+            response.sendError(
+                    HttpServletResponse.SC_FORBIDDEN, "You do not have permission to access this resource");
+            return;
+        }
+
+        //TODO
+
+//        int userId = Integer.parseInt(request.getParameter("userId"));
+//        int eventId = Integer.parseInt(request.getParameter("eventId"));
+//
+//        Attendance attendance = attendanceData.getAttendance(userId, eventId);
+//
+//        String url = String.format("/manager/view-reg?id=%d", eventId);
+//        String statusMessage;
+//
+//        if (attendance.getStatus() == Attendance.AttendanceStatus.NOT_ATTENDED) {
+//            if (EventHelpers.endsInFuture(attendance.getEvent())) {
+//
+//                int deregStatus = attendanceData.deregister(attendance);
+//
+//                // Check status code returned by deregister operation
+//                if (deregStatus == 0) {
+//                    statusMessage = "Registration has been removed";
+//                } else {
+//                    statusMessage = "ERROR: Delete event operation failed!";
+//                }
+//
+//            } else {
+//                statusMessage = "ERROR: You cannot de-register a guest from an event that has already taken place!";
+//            }
+//        } else {
+//            statusMessage = "ERROR: You cannot deregister a guest if they have already signed in!";
+//        }
+//
+//        request.setAttribute("statusMessage", statusMessage);
+//        RequestDispatcher view = request.getRequestDispatcher(url);
+//        view.forward(request, response);
+
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/remove-registration-guest.jsp
@@ -0,0 +1,25 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="/WEB-INF/templates/header.jsp"></jsp:include>
+
+<p>This will <strong>remove</strong> your registration from the event ${event.name} on ${eventLongDate}.</p>
+<p>${warningMessage}</p>
+
+<form action="remove-registration-guest" method="post">
+    <p>
+        Are you sure?<br>
+        <input type="hidden" name="eventId" value="${event.id}"><br>
+        <input type="submit" value="remove registration">
+        <button onclick="history.go(-1)">cancel</button>
+    </p>
+</form>
+
+<hr>
+
+<button onclick="history.go(-1)">back</button>
+
+</body>
+</html>
+
+
