b662e6acce729eca9b74f8e230e34da04ddad61d
Mike Molenda <molenda.mike@gmail.com>
2017-04-26 09:47:59 -0500

    Restrict access by non-Organizers

--- src/main/java/com/cis498/group4/controllers/UpdatePassword.java
+++ src/main/java/com/cis498/group4/controllers/UpdatePassword.java
@@ -38,10 +38,10 @@ public class UpdatePassword extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Restrict access if not logged in
-        if (!SessionHelpers.checkLogin(request.getSession())) {
+        // Restrict access by non-Organizers
+        if (!SessionHelpers.checkOrganizer(request.getSession())) {
             response.sendError(
-                    HttpServletResponse.SC_FORBIDDEN, "You must login to access this resource");
+                    HttpServletResponse.SC_FORBIDDEN, "You do not have permission to access this resource");
             return;
         }
 
@@ -69,10 +69,10 @@ public class UpdatePassword extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Restrict access if not logged in
-        if (!SessionHelpers.checkLogin(request.getSession())) {
+        // Restrict access by non-Organizers
+        if (!SessionHelpers.checkOrganizer(request.getSession())) {
             response.sendError(
-                    HttpServletResponse.SC_FORBIDDEN, "You must login to access this resource");
+                    HttpServletResponse.SC_FORBIDDEN, "You do not have permission to access this resource");
             return;
         }
 
