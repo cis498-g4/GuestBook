a6a8ee5588719aa478e5eea87d928e26477a0562
Mike Molenda <molenda.mike@gmail.com>
2017-05-08 16:20:59 -0500

    Start implementing registration checks

--- src/main/java/com/cis498/group4/controllers/AddRegistration.java
+++ src/main/java/com/cis498/group4/controllers/AddRegistration.java
@@ -3,8 +3,10 @@ package com.cis498.group4.controllers;
 import com.cis498.group4.data.AttendanceDataAccess;
 import com.cis498.group4.data.EventDataAccess;
 import com.cis498.group4.data.UserDataAccess;
+import com.cis498.group4.models.Attendance;
 import com.cis498.group4.models.Event;
 import com.cis498.group4.models.User;
+import com.cis498.group4.util.AttendanceHelpers;
 import com.cis498.group4.util.SessionHelpers;
 
 import javax.servlet.RequestDispatcher;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * The AddRegistration servlet responds to requests to register users for an event.
@@ -83,19 +86,70 @@ public class AddRegistration extends HttpServlet {
             return;
         }
 
+        String url;
+        String pageTitle;
         String statusMessage;
         String statusType;
 
         User user = userData.getUserByEmail(request.getParameter("email"));
         Event event = eventData.getEvent(Integer.parseInt(request.getParameter("eventId")));
         String eventDate = event.getStartDateTime().format(DateTimeFormatter.ofPattern("M/d/YY"));
-        String pageTitle = String.format("Registration for %s %s", event.getName(), eventDate);
 
-        request.setAttribute("event", event);
-        request.setAttribute("eventDate", eventDate);
-        request.setAttribute("pageTitle", pageTitle);
+        url = String.format("/WEB-INF/views/add-registration.jsp?id=%s", event.getId());
+        pageTitle = String.format("Registration for %s %s", event.getName(), eventDate);
 
-        String url = String.format("/WEB-INF/views/add-registration.jsp?id=%s", event.getId());
+        // Get existing user registrations
+        List<Attendance> registrations = attendanceData.getFutureRegistrations(user);
+
+        // Perform appropriate registration action / respond with appropriate message
+        int status = AttendanceHelpers.registerStatus(user, event, registrations);
+
+        switch(status) {
+            case AttendanceHelpers.SUCCESS:
+                int insertStatus = attendanceData.insertAttendance(user, event);
+                if (insertStatus == 0) {
+                    statusMessage = String.format("Successfully %s %s registered for %s!",
+                            user.getFirstName(), user.getLastName(), event.getName());
+                    statusType = "success";
+// TODO
+                } else if (insertStatus == 1062) {
+                    statusMessage = String.format("You are already registered for %s!", event.getName());
+                    statusType = "warning";
+                } else {
+                    statusMessage = String.format(
+                            "<strong>Error!</strong> There was a problem processing your registration %d", insertStatus);
+                    statusType = "danger";
+                }
+                break;
+            case AttendanceHelpers.ACTION_CLOSED_REGISTRATION:
+                statusMessage = String.format("<strong>Error!</strong> %s does not have open registration. " +
+                        "Please contact an event organizer to register.", event.getName());
+                statusType = "danger";
+                break;
+            case AttendanceHelpers.FAIL_EVENT_ENDED:
+                statusMessage = String.format("<strong>Error!</strong> %s has already concluded.", event.getName());
+                statusType = "danger";
+                break;
+            case AttendanceHelpers.FAIL_EVENT_FULL:
+                statusMessage = String.format("<strong>Error!</strong> The event %s is at capacity.", event.getName());
+                statusType = "danger";
+                break;
+            case AttendanceHelpers.FAIL_INVALID_EVENT:
+                statusMessage = String.format(
+                        "<strong>Error!</strong> No event was found with the registration code %s", registrationCode);
+                statusType = "danger";
+                break;
+            case AttendanceHelpers.FAIL_REG_OVERLAP:
+                statusMessage = String.format(
+                        "<strong>Error!</strong> %s overlaps one of your existing registrations.", event.getName());
+                statusType = "danger";
+                break;
+            default:
+                statusMessage = "<strong>Error!</strong> There was a problem processing your registration.";
+                statusType = "danger";
+                break;
+        }
+
 
         if (user.getEmail() != null) {
             int insertStatus = attendanceData.insertAttendance(user, event);
@@ -114,8 +168,12 @@ public class AddRegistration extends HttpServlet {
             statusType = "danger";
         }
 
+        request.setAttribute("event", event);
+        request.setAttribute("eventDate", eventDate);
+        request.setAttribute("pageTitle", pageTitle);
         request.setAttribute("statusMessage", statusMessage);
         request.setAttribute("statusType", statusType);
+
         RequestDispatcher view = request.getRequestDispatcher(url);
         view.forward(request, response);
 
